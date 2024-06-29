c1e0a13e1c4a7f97684b13ddac184020057da60e
Cmte-Kirk <kirk.java@gmail.com>
2024-06-29 04:51:50 -0300

    update .gitignore form **/__pycache__/

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,15 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
-from http import HTTPStatus
-
 
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
-    cliente = TestClient(app) #Arrange (organização)
+    cliente = TestClient(app)  # Arrange (organização)
 
-    response = cliente.get("/") #Act (acao)
+    response = cliente.get('/')  # Act (acao)
 
-    #Assert (verificacao)
+    # Assert (verificacao)
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá mundo!"}
+    assert response.json() == {'message': 'Olá mundo!'}
